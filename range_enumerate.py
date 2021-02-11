0818ba26ad175d1f8ad475d37ac0900e4039635f
Samuelkb <samuelk.b.hc@gmail.com>
2021-02-11 08:29:26 -0600

    solution with range

---
+++ range_enumerate.py
@@ -0,0 +1,27 @@
+# Coding question fizz_buzz
+def fizz_buzz(numbers):
+    '''
+    :param numbers: The list of numbers
+    :type numbers: list
+    Given a list of integers:
+    1. Replace all integers that are evenly divisible by 3 with "fizz"
+    2. Replace all integers divisible by 5 with "buzz"
+    3. Replace all integers divisible by both 3 and 5 with "fizzbuzz"
+    >>> numbers = [45, 22, 14, 65, 97, 72]
+    >>> fizz_buzz(numbers)
+    >>> numbers
+    ['fizzbuzz', 22, 14, 'buzz', 97, 'fizz']
+    '''
+
+    # Range way
+    for i in range(len(numbers)):
+        num = numbers[i]
+        if num % 3 == 0:
+            numbers[i] = "fizz"
+        if num % 5 == 0:
+            numbers[i] = "buzz"
+        if num % 3 == 0 and num % 5 == 0:
+            numbers[i] = "fizzbuzz"
+
+    # We will use the module doctest with the command `python3 -m doctest range_enumerate.py`
+    #That will be looking at your doc string  took tests, and run them and compare the output
