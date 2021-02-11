38de711d6350f9ebc1ce862d19fed8bdcde8c502
Samuelkb <samuelk.b.hc@gmail.com>
2021-02-11 08:38:28 -0600

    enumerate solution

--- range_enumerate.py
+++ range_enumerate.py
@@ -14,14 +14,29 @@ def fizz_buzz(numbers):
     '''
 
     # Range way
-    for i in range(len(numbers)):
-        num = numbers[i]
+    #for i in range(len(numbers)):
+    #    num = numbers[i]
+    #    if num % 3 == 0:
+    #        numbers[i] = "fizz"
+    #    if num % 5 == 0:
+    #        numbers[i] = "buzz"
+    #    if num % 3 == 0 and num % 5 == 0:
+    #        numbers[i] = "fizzbuzz"
+
+    # We will use the module doctest with the command `python3 -m doctest range_enumerate.py`
+    # That will be looking at your doc string  took tests, and run them and compare the output
+
+    # enumerate() is a fuction that takes in iterable and iterates through that iterable
+    # and returns tuples where the first element of the tuple is the index and the second
+    # element is the value.
+
+    # [tup for tup in enumerate([1, 2, 3])] -> [(0, 1), (1, 2), (2, 3)]
+    # [tup for tup in enumerate([1, 2, 3], start=10)] -> [(10, 1), (11, 2), (12, 3)]
+
+    for i, num in enumerate(numbers):
         if num % 3 == 0:
             numbers[i] = "fizz"
         if num % 5 == 0:
             numbers[i] = "buzz"
         if num % 3 == 0 and num % 5 == 0:
             numbers[i] = "fizzbuzz"
-
-    # We will use the module doctest with the command `python3 -m doctest range_enumerate.py`
-    #That will be looking at your doc string  took tests, and run them and compare the output
